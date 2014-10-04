61b7d7f1611108ce25db9983e533c2cbfce11e5b
Lars Rehn <rehn.lars@gmail.com>
2014-10-04 23:17:46 +0200

    switch if

--- button.go
+++ button.go
@@ -19,18 +19,18 @@ func NewButton(gpioPin int, repeatIterval int64) Button {
 
 func (b Button) buttonWatcher() {
 	currentValue := b.Gp.GetValue()
+
 	for {
 		newValue := b.Gp.GetValue()
 		if currentValue != newValue || newValue == true {
-			if newValue == true {
+			if newValue == false {
+				b.ButtonUp <- true
+			} else {
 				b.ButtonDown <- true
-				log.Print("ButtonDown")
 				var d time.Duration = time.Duration(time.Duration(b.RepeatInterval-10) * time.Millisecond)
 				time.Sleep(d)
-			} else {
-				log.Print("ButtonUp")
-				b.ButtonUp <- true
 			}
+
 			currentValue = newValue
 		}
 		time.Sleep(10 * time.Millisecond)
